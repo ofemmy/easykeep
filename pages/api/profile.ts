83fcb67bd79293c8375e432392f8151ab6974b40
Oladayo Fagbemi <oofagbemi@outlook.com>
2021-03-15 01:30:44 +0100

    settings category done

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import prisma from "../../db/prisma";
 import { DefaultCategories } from "../../store";
+import capitalize from "../../lib/capitalize";
 
 const handler = nc<NextApiRequest, NextApiResponse>({
   onNoMatch(req, res) {
@@ -26,11 +27,66 @@ handler.post(
             categories: DefaultCategories,
           },
         });
-        res.status(201).send({ status: "success", data: result });
+        res.status(201).send({ status: "success", profile: result });
       } catch (error) {
         res.status(500).send({ error: "Internal server error" });
       }
     }
+  })
+);
+handler.get(
+  withApiAuthRequired(async (req, res) => {
+    const user = getSession(req, res);
+    try {
+      const userProfile = await prisma.profile.findFirst({
+        where: { ownerId: user.sub },
+      });
+      res.status(200).send({ msg: "success", profile: userProfile });
+    } catch (error) {}
+  })
+);
+handler.patch(
+  withApiAuthRequired(async (req, res) => {
+    const { type } = req.query;
+    const { action, payload } = req.body;
+    const { user } = getSession(req, res);
+    try {
+      const userProfile = await prisma.profile.findFirst({
+        where: { ownerId: user.sub },
+      });
+      let result, message;
+      if (type === "categories") {
+        switch (action) {
+          case "add":
+            let cat = new Set(userProfile.categories);
+            cat.add(payload.newCategory);
+            const updatedList = [...Array.from(cat)];
+            result = await prisma.profile.update({
+              where: { id: userProfile.id },
+              data: {
+                categories: updatedList,
+              },
+            });
+            message = `${capitalize(payload.newCategory)} added to categories`;
+            break;
+          case "delete":
+            const filteredList = userProfile.categories.filter(
+              (c) => c !== payload.newCategory
+            );
+            result = await prisma.profile.update({
+              where: { id: userProfile.id },
+              data: {
+                categories: filteredList,
+              },
+            });
+            message = `${capitalize(
+              payload.newCategory
+            )} removed from categories`;
+            break;
+        }
+      }
+      res.status(200).send({ msg: message, data: result });
+    } catch (error) {}
   })
 );
 export default handler;
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -3,49 +3,101 @@ import React, { useContext, useState } from "react";
 import DeleteSVG from "../components/DeleteSVG";
 import Header from "../components/Header";
 import EditSVG from "../components/svgs/EditSVG";
-
+import axios from "axios";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import { MyAppContext } from "../store";
-const CategoryComponent = () => {
-  const { categories, addCategory } = useContext(MyAppContext);
+import { useToast } from "@chakra-ui/react";
+const CategoryComponent = ({ data }) => {
+  // const { categories, addCategory } = useContext(MyAppContext);
+  const queryClient = useQueryClient();
   const [category, setCategory] = useState("");
-  const [isEditMode, setIsEditMode] = useState(false);
   const [hoveredCategory, setHoveredCategory] = useState("");
-  const setEditMode = (category) => {
-    setIsEditMode(true);
-    setCategory(category);
+  const toast = useToast();
+  const mutation = useMutation(
+    async ({ newCategory, action }: any) => {
+      const response = await axios.patch("/api/profile?type=categories", {
+        payload: { newCategory },
+        action,
+      });
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        toast({
+          description: data.msg,
+          status: "success",
+          duration: 2000,
+          position: "top",
+        });
+      },
+      onMutate: async ({ newCategory }) => {
+        await queryClient.cancelQueries("profile");
+        const previousProfile = (await queryClient.getQueryData(
+          "profile"
+        )) as any;
+        queryClient.setQueryData("profile", (old: any) => {
+          const oldCat = previousProfile.profile.categories;
+          oldCat.push(newCategory);
+          return old;
+        });
+        return { previousProfile };
+      },
+      onError: (err, newProfile, context: any) => {
+        queryClient.setQueryData("profile", context.previousProfile);
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries("profile");
+      },
+    }
+  );
+  const addNewCategory = () => {
+    if (category) {
+      mutation.mutate({ newCategory: category, action: "add" });
+    }
   };
+  const deleteCategory = (category) => {
+    mutation.mutate({ newCategory: category, action: "delete" });
+  };
+  // const setEditMode = (category) => {
+  //   setIsEditMode(true);
+  //   setCategory(category);
+  // };
   return (
     <div className="overflow-y-auto">
       <div className="flex justify-between">
         <ul className=" flex-1 bg-white rounded-sm shadow-sm">
-          {categories.map((category) => (
-            <li
-              onMouseEnter={() => {
-                setHoveredCategory(category);
-              }}
-              onMouseLeave={() => {
-                setHoveredCategory("");
-              }}
-              className="py-4 px-4 hover:bg-yellow-100 odd:bg-yellow-50 flex justify-between items-center"
-            >
-              <p>{capitalize(category)}</p>
-              <div
-                className={`${
-                  hoveredCategory == category ? "flex" : "hidden"
-                } space-x-2`}
+          {data.categories.length > 0 ? (
+            data.categories.map((category) => (
+              <li
+                onMouseEnter={() => {
+                  setHoveredCategory(category);
+                }}
+                onMouseLeave={() => {
+                  setHoveredCategory("");
+                }}
+                className="py-4 px-4 hover:bg-yellow-100 odd:bg-yellow-50 flex justify-between items-center"
               >
-                <button className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                  <DeleteSVG />
-                </button>
-                <button
-                  onClick={() => setEditMode(category)}
-                  className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                <p>{capitalize(category)}</p>
+                <div
+                  className={`${
+                    hoveredCategory == category ? "flex" : "hidden"
+                  } space-x-2`}
                 >
-                  <EditSVG />
-                </button>
-              </div>
+                  <button
+                    onClick={() => deleteCategory(category)}
+                    className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                  >
+                    <DeleteSVG />
+                  </button>
+                </div>
+              </li>
+            ))
+          ) : (
+            <li className="py-4 px-4 hover:bg-yellow-100 odd:bg-yellow-50 flex justify-between items-center">
+              No category yet
             </li>
-          ))}
+          )}
         </ul>
         <div className="bg-white rounded-md shadow-sm ml-8 w-5/12 h-52 py-8 px-4">
           <div>
@@ -53,7 +105,7 @@ const CategoryComponent = () => {
               htmlFor="new-category"
               className="block text-sm font-medium text-gray-700"
             >
-              {isEditMode ? "Update Category" : "Add New Category"}
+              Add New Category
             </label>
             <div className="mt-2">
               <input
@@ -65,11 +117,11 @@ const CategoryComponent = () => {
                 onChange={(e) => setCategory(e.target.value)}
               />
               <button
-                onClick={() => addCategory(category)}
+                onClick={addNewCategory}
                 type="button"
                 className="mt-4 inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
               >
-                {isEditMode ? "Update" : "Add New Category"}
+                Add New Category
               </button>
             </div>
           </div>
@@ -78,15 +130,32 @@ const CategoryComponent = () => {
     </div>
   );
 };
-const Comp2 = () => <p>Comp2</p>;
-export default function Settings() {
+const Comp2 = ({ data }) => <p>Comp2</p>;
+
+export default withPageAuthRequired(function Settings() {
   const { language, currency } = useContext(MyAppContext);
   const pageLinks = ["category", "language", "currency"];
   const [activeLink, setActiveLink] = useState("category");
-  const ActivePage = ({ activeLink }) => {
-    if (activeLink == "category") return <CategoryComponent />;
-    if (activeLink == "language") return <Comp2 />;
+  const ActivePage = ({ activeLink, data }) => {
+    if (activeLink == "category") return <CategoryComponent data={data} />;
+    if (activeLink == "language") return <Comp2 data={data} />;
   };
+  const fetchUserProfile = async () => {
+    const response = await axios.get("/api/profile");
+    return response.data;
+  };
+  const { data, isLoading, isError, error } = useQuery(
+    "profile",
+    fetchUserProfile,
+    { refetchOnWindowFocus: false }
+  );
+  if (isLoading) {
+    return <span>Loading....</span>;
+  }
+  if (isError) {
+    return <span>Error: {error}</span>;
+  }
+  const { profile } = data;
   return (
     <div className="max-6xl mx-auto border-t border-gray-200">
       <Header pageTitle={"Settings"} />
@@ -126,11 +195,11 @@ export default function Settings() {
               </nav>
             </div>
             <div className="mt-10">
-              {<ActivePage activeLink={activeLink} />}
+              {<ActivePage activeLink={activeLink} data={profile} />}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+});
